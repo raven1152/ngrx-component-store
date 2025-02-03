c78b642e6d2130fb452ff2cbe3a8dcfc7530b2af
D Preston <services@davidjpreston.info>
2025-02-02 22:52:27 -0600

    added some more fields

--- src/app/user-status/user-status.component.ts
+++ src/app/user-status/user-status.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserStore } from '../+state/user.state';
 import { tap } from 'rxjs';
+import { UserAvailability } from '../+state/user.state';
 
 @Component({
   selector: 'ogs-user-status',
@@ -8,7 +9,7 @@ import { tap } from 'rxjs';
   templateUrl: './user-status.component.html',
   styleUrl: './user-status.component.scss',
 })
-export class UserStatusComponent {
+export class UserStatusComponent implements OnInit {
   private store = inject(UserStore);
   public isAuthenticated$ = this.store.isAuthenticated$.pipe(
     tap(value => console.log('UserStatusComponent/tap/isAuthenticated', value)),
@@ -16,4 +17,19 @@ export class UserStatusComponent {
   public state$ = this.store.state$.pipe(
     tap(value => console.log('UserStatusComponent/tap/state', value)),
   );
+  public availabilities: { key: string, value: string }[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.availabilities = this.getAvailabilities();
+  }
+
+  private getAvailabilities() {
+    let arr = [];
+    for (const [key, value] of Object.entries(UserAvailability)) {
+      arr.push({ key, value });
+    }
+    return arr;
+  }
 }
